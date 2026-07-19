220641ab62c48b0ba2641c3283a0fc991efb4419
sMv_Production <sMv_Developer@outlook.com>
2026-07-19 21:00:05 +0530

    Update server.js

--- backEnd/src/server.js
+++ backEnd/src/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Server setting
 app.set('trust proxy', 1); // Use the number of proxies your server sits behind
-app.get('/ping', (req, res) => {
+app.get('/ping', cors({ origin: '*' }), (req, res) => {
   res.status(200).send('success');
 });
 
